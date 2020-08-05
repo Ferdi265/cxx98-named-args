12007f7f4b2af0ab31449ca538258cfabccbff8f
Ferdinand Bachmann <theferdi265@gmail.com>
2020-08-05 19:49:23 +0200

    initial test file

--- test.cpp
+++ test.cpp
@@ -1,15 +1,52 @@
-#include <cassert>
+#include <iostream>
+#include <string>
+#include "named_args.h"
 
-#include "tuple98.h"
-#include "tuple98_traits.h"
+// define argument types
+struct name_t : named_args::req_arg {};
+struct age_t : named_args::def_arg<int, -1>  {};
+struct bufsiz_t : named_args::def_arg<size_t, 4096> {};
+struct nice_t : named_args::def_arg<int*> {};
+
+// create named argument markers
+const named_args::marker<name_t> name;
+const named_args::marker<age_t> age;
+const named_args::marker<bufsiz_t> bufsiz;
+const named_args::marker<nice_t> nice;
+
+// implementation
+void test_impl(std::string name, int age, size_t bufsiz, int* nice) {
+    std::cout << "test:\n";
+
+    std::cout << "- name is " << name << "\n";
+
+    if (age != -1) {
+        std::cout << "- age is " << age << "\n";
+    } else {
+        std::cout << "- no age given\n";
+    }
+
+    std::cout << "- bufsiz is " << bufsiz << "\n";
+
+    if (nice) {
+        *nice = 42;
+    }
+}
+
+const named_args::function<void(*)(std::string, int, size_t, int*), &test_impl, name_t, age_t, bufsiz_t, nice_t> test;
+
+// tests
+void foo(char * s, int a, int b, int& n) {
+    test(name = s, age = a, bufsiz = b, nice = &n);
+}
 
 int main() {
-    TUPLE(int, int, float) nice = tuple98::make_tuple(1, 2, 3.14159f);
+    int n;
 
-    size_t pi1 = tuple98::get<2>(nice);
-    size_t pi2 = tuple98::get<float>(nice);
-    assert(pi1 == pi2);
+    test(name = "foo", age = 42, bufsiz = 8192);
+    test(bufsiz = 8192, name = "foo", age = 42);
+    test(name = "bar", age = 1337);
+    test(name = std::string("baz"), nice = &n);
 
-    size_t v1 = tuple98_traits::count<TUPLE(int, int, float), int>::value;
-    assert(v1 == 2);
+    std::cout << "nice! " << n << "\n";
 }
