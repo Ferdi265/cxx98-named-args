3784143c69632dc3c3af910193404186c4a1c4ba
Ferdinand Bachmann <theferdi265@gmail.com>
2020-08-05 13:09:41 +0200

    add test cases

--- test.cpp
+++ test.cpp
@@ -1,1 +1,15 @@
+#include <cassert>
+
 #include "tuple98.h"
+#include "tuple_traits.h"
+
+int main() {
+    TUPLE(int, int, float) nice = tuple98::make_tuple(1, 2, 3.14159f);
+
+    size_t pi1 = tuple98::get<2>(nice);
+    size_t pi2 = tuple98::get<float>(nice);
+    assert(pi1 == pi2);
+
+    size_t v1 = tuple_traits::count<TUPLE(int, int, float), int>::value;
+    assert(v1 == 2);
+}
